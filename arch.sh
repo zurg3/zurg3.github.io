2415b4901c3dba9f09113be244770ab955556d99
zurg3 <20664125+zurg3@users.noreply.github.com>
2020-03-05 18:26:29 +0300

    Edited arch.sh

--- arch.sh
+++ arch.sh
@@ -2,5 +2,5 @@
 
 wget -q https://raw.githubusercontent.com/zurg3/arch-linux-info/master/arch_linux_install.sh
 chmod +x arch_linux_install.sh
-rm $0
-mv arch_linux_install.sh arch.sh
+rm arch.sh
+./arch_linux_install.sh
